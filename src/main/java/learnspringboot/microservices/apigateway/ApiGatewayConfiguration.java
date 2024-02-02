52e0e1d988931603302b4b22da3697121fc4a554
Bohdan Danylov <bohdandanylov@gmail.com>
2024-02-01 20:42:11 -0800

    v1.3.2 - Implementing Spring Cloud Gateway Logging Filter

--- src/main/java/learnspringboot/microservices/apigateway/ApiGatewayConfiguration.java
+++ src/main/java/learnspringboot/microservices/apigateway/ApiGatewayConfiguration.java
@@ -18,19 +18,25 @@ public class ApiGatewayConfiguration {
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
 
         return builder.routes()
-                .route(p -> p.path("/get")
-                .filters(f -> f
-                        .addRequestHeader("MyHeader", "MyURI")
-                        .addRequestParameter("Parameter", "MyValue"))
-                .uri("http://httpbin.org:80"))
-                .route(p -> p.path("/currency-exchange/**")
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Parameter", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .path("/currency-exchange/**")
                         .uri("lb://currency-exchange"))
-                .route(p -> p.path("/currency-conversion/**")
+                .route(p -> p
+                        .path("/currency-conversion/**")
                         .uri("lb://currency-conversion"))
-                .route(p -> p.path("/currency-conversion-feign/**")
+                .route(p -> p
+                        .path("/currency-conversion-feign/**")
                         .uri("lb://currency-conversion"))
-                .route(p -> p.path("/currency-conversion-test/**")
-                        .filters(f -> f.rewritePath(
+                .route(p -> p
+                        .path("/currency-conversion-test/**")
+                        .filters(f -> f
+                                .rewritePath(
                                 "/currency-conversion-test/(?<segment>.*)",
                                 "/currency-conversion-feign/${segment}"))
                         .uri("lb://currency-conversion"))
